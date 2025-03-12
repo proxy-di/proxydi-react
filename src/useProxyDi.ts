27b28c812e16f74e8809825ff1d79b304b61f961
Yolziii <yolziii@gmail.com>
2025-03-12 20:04:08 +0200

    typed useProxyDi

--- src/useProxyDi.ts
+++ src/useProxyDi.ts
@@ -1,5 +1,5 @@
 import { useProxyDiContainer } from './useProxyDiContainer';
-import { DependencyId } from 'proxydi';
+import { DependencyClass, DependencyId } from 'proxydi';
 
 /**
  * Resolves a dependency from the ProxyDi container. A wrapper for the ProxyDiContainer.resolve() method.
@@ -11,12 +11,26 @@ import { DependencyId } from 'proxydi';
  * @throws {Error} If dependency ID is unknown.
  * @throws {Error} If the hook is used outside of a ProxyDiProvider.
  */
-export const useProxyDi = <T>(dependencyId: DependencyId): T => {
+export function useProxyDi<T>(dependencyId: DependencyId): T;
+/**
+ * Resolves a dependency from the ProxyDi container by class. A wrapper for the ProxyDiContainer.resolve() method.
+ *
+ * @template T - The dependency class.
+ * @param {T} SomeClass - The class to resolve.
+ * @returns {InstanceType<T>} The resolved dependency instance.
+ *
+ * @throws {Error} If class is not registered.
+ * @throws {Error} If the hook is used outside of a ProxyDiProvider.
+ */
+export function useProxyDi<T extends DependencyClass<any>>(
+  SomeClass: T,
+): InstanceType<T>;
+export function useProxyDi(dependencyIdOrClass: any): any {
   const container = useProxyDiContainer();
 
   if (!container) {
     throw new Error('useProxyDi must be used within a ProxyDiProvider');
   }
 
-  return container.resolve<T>(dependencyId);
-};
+  return container.resolve(dependencyIdOrClass);
+}
